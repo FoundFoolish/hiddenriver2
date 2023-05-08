91b2e114f370bb450603dc9353e0896ae2f56691
TippyWink <126517007+TippyWink@users.noreply.github.com>
2023-05-08 14:02:49 -0500

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-<script type="text/javascript">
-var password = "please";
-var x = prompt("Enter in the password "," ");
-if (x.toLowerCase() == password) {
- alert("Come right in \n \n You've entered in the right password");
- window.location = "good.htm";
-}
-else {
- window.location = "bad.htm";
-}
-</script>
+
